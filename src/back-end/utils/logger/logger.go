ce7b24da0a6852ea4438e0257f8737f9bac4014d
Jake Bailey - UP2002753 <jake.bailey2801@gmail.com>
2023-04-10 11:49:34 +0100

    go logger multiwriter (console and file)

--- src/back-end/utils/logger/logger.go
+++ src/back-end/utils/logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"time"
@@ -9,6 +10,20 @@ import (
 
 const (
 	LogsDir = "logs"
+
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	Gray   = "\033[37m"
+	White  = "\033[97m"
+)
+
+var (
+	mw = io.MultiWriter(os.Stdout, SetLogFile())
 )
 
 type LogDir struct {
@@ -30,22 +45,21 @@ func SetLogFile() *os.File {
 	hour, min, sec := time.Now().Clock()
 	fileName := fmt.Sprintf("%d-%d-%d-%d-%d-%d.log", day, month, year, hour, min, sec)
 	filePath, _ := os.OpenFile(LogsDir+"/"+fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-
 	return filePath
 }
 
 func (l *LogDir) Info() *log.Logger {
-	return log.New(SetLogFile(), "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	return log.New(mw, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func (l *LogDir) Warning() *log.Logger {
-	return log.New(SetLogFile(), "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	return log.New(mw, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func (l *LogDir) Error() *log.Logger {
-	return log.New(SetLogFile(), "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	return log.New(mw, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func (l *LogDir) Fatal() *log.Logger {
-	return log.New(SetLogFile(), "FATAL: ", log.Ldate|log.Ltime|log.Lshortfile)
+	return log.New(mw, "FATAL: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
