9181dc441faafed37cd13df91f082b18ab7ddca8
Jake Bailey - UP2002753 <jake.bailey2801@gmail.com>
2023-07-14 15:48:39 +0100

    Open Route Service support added instead of OSRM as it includes support for elevation data which OSRM doesn't supply.

--- src/client/src/Map/Map.jsx
+++ src/client/src/Map/Map.jsx
@@ -3,7 +3,7 @@ import RoutingMachine from './RoutingMachine';
 
 const Map = () => {
   return (
-    <MapContainer center={[50.798908,-1.091160]} zoom={13} scrollWheelZoom={true}>
+    <MapContainer id="map" center={[50.798908,-1.091160]} zoom={13} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/client/src/Map/RoutingMachine.jsx
+++ src/client/src/Map/RoutingMachine.jsx
@@ -4,13 +4,37 @@ import 'leaflet-routing-machine';
 import 'leaflet-control-geocoder/dist/Control.Geocoder.js'
 import 'leaflet-control-geocoder/dist/Control.Geocoder.css'
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
+import '@gegeweb/leaflet-routing-machine-openroute/dist/leaflet-routing-openroute.min.js'
+
+import './Map.css'
 
 const createRoutingMachineLayer = (props) => {
-  const router = L.Routing.osrmv1({
-    serviceUrl: 'http://localhost:5000/route/v1',
-    profile: 'bike',
-    useHints: false,
+  // const router = L.Routing.osrmv1({
+  //   serviceUrl: 'http://localhost:5000/route/v1',
+  //   profile: 'bike',
+  //   useHints: false,
+  // })
+
+  const apiKey = "5b3ce3597851110001cf624804aafa7570224300b37f2f457b2d5438";
+  const router = new L.Routing.OpenRouteService(apiKey, {
+    timeout: 30 * 1000, // 30",
+        format: "json",                           // default, gpx not yet supported
+        host: "https://api.openrouteservice.org", // default if not setting
+        service: "directions",                    // default (for routing) 
+        api_version: "v2",                        // default
+        profile: "cycling-road",                  // default
+        routingQueryParams: {
+            attributes: [
+                "avgspeed",
+                "percentage",
+            ],
+            language: "en",
+            maneuvers: "true",
+            preference: "recommended",
+            elevation: "true",
+        },
   })
+
 
   const instance = L.Routing.control({
     router,
@@ -31,7 +55,18 @@ const createRoutingMachineLayer = (props) => {
     fitSelectedRoutes: false,
     showAlternatives: true,
     geocoder: L.Control.Geocoder.nominatim(),
+    collapsible: true,
   });
+
+  instance.on('routesfound', (e) => {
+    const routes = e.routes;
+    const summary = routes[0].summary;
+    alert(routes);
+    console.log(routes);
+    console.log(`Total ascent: ${summary.totalAscend} m`);
+    console.log(`Total descent: ${summary.totalDescend} m`);
+  });
+
   return instance;
 }
 
