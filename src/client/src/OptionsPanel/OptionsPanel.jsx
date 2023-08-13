26114fc8d1eafd07b796027201e16feccf9ea679
jaketbailey <jake@jaketbailey.co.uk>
2023-08-13 14:14:31 +0100

    minor styling changed on geojson button and gpx button added to match geojson button, functionality not yet implemented

--- src/client/src/OptionsPanel/OptionsPanel.jsx
+++ src/client/src/OptionsPanel/OptionsPanel.jsx
@@ -1,7 +1,7 @@
 import './OptionsPanel.css';
 
 const OptionsPanel = (props) => {
-    const saveGeoJSON = () => {
+    const saveGeoJSON = (type) => {
         props.geoJSONLink.click();
     }
 
@@ -11,6 +11,9 @@ const OptionsPanel = (props) => {
                <div> 
                     <button className="options-panel__button" onClick={saveGeoJSON} >
                          <img src="/img/save-route/geojson-file.svg" alt="Save Route as GeoJSON" title="Save Route as GeoJSON"/>    
+                    </button>
+                    <button className="options-panel__button" onClick={saveGeoJSON} >
+                         <img src="/img/save-route/gpx-file.svg" alt="Save Route as GPX" title="Save Route as GPX"/>    
                     </button> 
                 </div> 
             </div>
