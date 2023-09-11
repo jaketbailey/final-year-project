fdd732a44f15253e9cca5ffc8b3dadc52dc9254e
jaketbailey <jake@jaketbailey.co.uk>
2023-09-11 14:34:43 +0100

    Added day to weekly forecast

--- src/client/src/WeatherPanel/DailyForecast.jsx
+++ src/client/src/WeatherPanel/DailyForecast.jsx
@@ -1,12 +1,21 @@
 import './DailyForecast.css'
 
 const DailyForecast = (props) => {
+  const getDayString = (index) => {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return days[index];
+  }
 
   const DailyItem = (props) => {
     const getFullDate = () => {
       const date = new Date(props.day.dt * 1000);
-      console.log(date)
-      return `${date.getDate()}/${date.getMonth() + 1}`
+      return (
+        <>
+            {getDayString(date.getDay())}
+            <br/>
+            {`${date.getDate()}/${date.getMonth() + 1}`}
+        </>
+      )
     }
 
     const getIcon = () => {
@@ -17,20 +26,17 @@ const DailyForecast = (props) => {
       <div id={`daily-item-${props.id}`} className="daily-item">
           <div className="daily-details">{getFullDate()}</div>
           <div className="daily-details">{getIcon()}</div>
-          <div className="daily-details">{Math.round(props.day.temp.day - 273.15)}°C</div>
+          <div className="daily-details">{`${Math.round(props.day.temp.min - 273.15)}-${Math.round(props.day.temp.max - 273.15)}`}°C</div>
           <div className="daily-details">{props.day.weather[0].main}</div>
       </div>
     )
   }
-
-  console.log(props.daily)
 
   const forecastArray = [];
 
   if (!props.daily) return;
 
   for (const [i, day] of props.daily.entries()) {
-    console.log(i)
     if ((i > 0) && (i < 7)) {
       forecastArray.push(<DailyItem id={i} day={day} />);
     }
